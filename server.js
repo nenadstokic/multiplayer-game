8c3b56dc10dee28fa3b0a1de79b124a01e4859b0
Nenad Stokic <nstokic@gmail.com>
2020-04-28 22:57:10 +0200

    connecting, disconnecting players - part 1 done

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
+
+let players = {};
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('A user ' + socket.id + ' connected.');
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: Math.floor(Math.random() * 700) + 50,
+    y: Math.floor(Math.random() * 500) + 50,
+    playerId: socket.id,
+    team: Math.floor(Math.random() * 2) == 0 ? 'red' : 'blue'
+  };
+  // send players object to new player
+  socket.emit('currentPlayers', players);
+  // send the new player data to all other players
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+
+  socket.on('disconnect', () => {
+    console.log('User ' + socket.id + ' disconnected.');
+    // remove this player from players object
+    delete players[socket.id];
+    // emmit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
+  });
+});
+
+server.listen(8081, () => {
+  console.log(`Listening on ${server.address().port}`);
+});
